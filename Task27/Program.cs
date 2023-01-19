42fa1e800cd3638f3f6d61494237b36b740707f7
Barbara <varya.sheptunova@gmail.com>
2023-01-19 10:52:33 +0300

    Task 27. Sum of digits in a number

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+int GetSum(int number)
+{
+    int result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+
+    return result;
+}
+
+WriteLine("Input number: ");
+
+int userNumber = int.Parse(ReadLine()!);
+
+int sumDigits = GetSum(userNumber);
+
+WriteLine($"Сумма цифр в числе: {sumDigits}");
