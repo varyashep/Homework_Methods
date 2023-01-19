db5fa9da19a2234393103961c6546cb23d7c6a43
Barbara <varya.sheptunova@gmail.com>
2023-01-19 10:58:41 +0300

    Task 29. Random array of 8 integers

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Массив случайных чисел из 8 элементов 
+using static System.Console;
+
+void RandomArray(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        Random rnd = new Random();
+        array[i] = rnd.Next();
+        Write(array[i] + " ");
+    }
+}
+
+RandomArray(8);
