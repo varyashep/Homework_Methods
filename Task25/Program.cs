bb6df3897ff0fd4a52206517058f21a6ee8205ff
Barbara <varya.sheptunova@gmail.com>
2023-01-19 10:47:00 +0300

    Task 25. A in degree B

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Метод принимает на вход два числа A и B и возводит А в степень B
+using static System.Console;
+
+int GetDegree(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 0; i < number2; i++)
+    {
+        result *= number1;
+    }
+    return result;
+}
+
+WriteLine("Input number A: ");
+
+int A = int.Parse(ReadLine()!);
+
+WriteLine("Input number B: ");
+
+int B = int.Parse(ReadLine()!);
+
+int finalNumber = GetDegree(A,B);
+
+WriteLine($"Final number: {finalNumber}");
