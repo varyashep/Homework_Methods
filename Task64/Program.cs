0f65a452f3fe31d3ad2db983a8eefb0cdb7497b2
Barbara <varya.sheptunova@gmail.com>
2023-02-05 13:52:37 +0300

    Task 64. Numbers from n to 1

---
+++ Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int n = int.Parse(ReadLine()!);
+WriteLine($"Числа от {n} до 1: ");
+GetNumbers(n);
+
+void GetNumbers(int num)
+{
+    if (num==1) Write(num);
+    else
+    {
+        Write($"{num},");
+        GetNumbers(num-1);
+    }
+}
