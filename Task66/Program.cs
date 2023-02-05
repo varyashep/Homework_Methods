95b0e96657b2dc9aa3bd1e70abdc9024fb3d9fd7
Barbara <varya.sheptunova@gmail.com>
2023-02-05 14:24:02 +0300

    Task 66. Sum of numbers from m to n

---
+++ Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+WriteLine("Введите два числа через пробел: ");
+int[] parameters = Array.ConvertAll(ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int sum = GetSum(parameters[0], parameters[1]);
+WriteLine($"Сумма чисел от {parameters[0]} до {parameters[1]}: {sum}");
+
+int GetSum(int m, int n)
+{
+    int result = 0;
+    if (n==m) return result + m;
+    else
+    {
+        result += n;
+        return result + GetSum(m,n-1);
+    }
+}
